8899c43794a35325126dad507167cf84b6d479d1
gokarna123-goku <agokarna444@gmail.com>
2023-06-24 10:35:10 +0545

    working on the navbar fix

---
+++ main.js
@@ -0,0 +1,13 @@
+
+window.addEventListener("scroll", function(){
+    var navbar = document.querySelector("nav");
+    var scrollY = window.scrollY;
+
+    if(scrollY >= 100){
+        navbar.classList.add("fixed");
+        navbar.style.transition = "0.5s ease-out";
+    } else{
+        navbar.classList.remove("fixed");
+        navbar.style.transition = "0.5s ease-out";
+    }
+})
