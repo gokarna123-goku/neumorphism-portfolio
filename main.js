218875f753f6f3ab6243633a8e185839c5bcd839
gokarna123-goku <agokarna444@gmail.com>
2023-06-25 15:06:35 +0545

    working on the section

--- main.js
+++ main.js
@@ -22,4 +22,24 @@ $(document).ready(function () {
 });
 
 
+// Active while scrolling
+// let sections = document.querySelectorAll(".section");
+// let navLink = document.querySelectorAll("nav ul li a");
+
+// window.onscroll = () => {
+//     sections.forEach(box = () => {
+//         let top = window.scrollY;
+//         let offset = box.offsetTop;
+//         let height = box.offsetHeight;
+//         let id = box.getAttribute("id");
+
+//         if(top >= offset && top < offset + height){
+//             navLink.forEach(links = () => {
+//                 links.classList.remove("active");
+//                 document.querySelector('nav ul li a[href*=' + id + ']').classList.add("active");
+//             });
+//         }
+//     });
+// };
+
 // 
